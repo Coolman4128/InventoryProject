f55e688bbbb7e1542ae225e0751c0a566954cb76
Coolman4128 <tylordgaming@gmail.com>
2024-06-15 17:42:49 -0400

    Add models and started the basic steps to display and change those models

--- tools/models.py
+++ tools/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import datetime 
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tool(models.Model):
+    name = models.CharField(max_length=255, unique = True)
+    barcodeID = models.CharField(max_length=255, unique=True)
+    isCheckedOut = models.BooleanField()
+    timeCheckedOut = models.DateTimeField(null=True, blank=True)
+    userCheckedOut = models.CharField(max_length=50, blank=True)
+    location = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+
+class Supply(models.Model):
+    name = models.CharField(max_length=255, unique = True)
+    barcodeID = models.CharField(max_length=255, unique=True)
+    quantityReplenished = models.IntegerField()
+    isLow = models.BooleanField(db_default=False)
+    lastReplenished = models.DateTimeField(auto_now_add=True)
+    whoReplenished = models.CharField(max_length=50)
+    location = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+class InvUser(models.Model):
+    name = models.CharField(max_length=50, unique = True)
+
+    def __str__(self):
+        return self.name
+
+
--- tools/views.py
+++ tools/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tool
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    tools = Tool.objects.all()
+    return render(request, 'home.html', {'tools': tools})
+
+
+
+
+ 
